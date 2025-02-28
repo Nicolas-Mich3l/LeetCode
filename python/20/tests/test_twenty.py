713b5a20134b037d5cb2eda9c537c60ceecad3c4
Nicolas-Mich3l <nicolas.michel@uconn.edu>
2025-02-28 15:46:56 -0500

    tests

--- python/20/tests/test_twenty.py
+++ python/20/tests/test_twenty.py
@@ -1,12 +1,23 @@
-from isValidParens.twenty import Solution
+from leetcode20_ndam.twenty import Solution
+import pytest
 
-def test_Matching():
+def test_Empty():
     implementation = Solution()
     assert implementation.isValid("") is True
+
+def test_Correct():
+    implementation = Solution()
     assert implementation.isValid("(){}[]") is True
     assert implementation.isValid("([{}])") is True
     assert implementation.isValid("({[]}[]([]{}))") is True
-    assert implementation.isValid("(") is False
-    assert implementation.isValid("{]") is False
-    assert implementation.isValid("([)]") is False
-    assert implementation.isValid(")") is False
+    
+@pytest.mark.parametrize("test_input,expected",[
+    ("(",False),
+    ("{]",False),
+    ("([}])",False),
+    (")",False),
+    ])
+
+def test_Incorrect(test_input,expected):
+    implementation = Solution()
+    implementation.isValid(test_input) is expected
